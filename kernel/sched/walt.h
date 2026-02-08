03834e595be3e4759cedac6646778d75e249af2b
SenKU <miquu.official@gmail.com>
2026-02-08 15:49:13 +0700

    Hardcoded DEFAULT_SCHED_RAVG_WINDOW to 16.66ms (16666666\text{ ns}). and Reduced EARLY_DETECTION_DURATION from 9.5ms to 5ms.

--- kernel/sched/walt.h
+++ kernel/sched/walt.h
@@ -18,10 +18,10 @@
  * Tick interval becomes to 3333333 due to
  * rounding error when HZ=300.
  */
-#define DEFAULT_SCHED_RAVG_WINDOW (16666666 * 6)
+#define DEFAULT_SCHED_RAVG_WINDOW (16666666)
 #else
 /* Default window size (in ns) = 12ms */
-#define DEFAULT_SCHED_RAVG_WINDOW 12000000
+#define DEFAULT_SCHED_RAVG_WINDOW 16000000
 #endif
 
 /* Max window size (in ns) = 1s */
