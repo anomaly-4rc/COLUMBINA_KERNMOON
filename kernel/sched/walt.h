cf3a42528fe9c4a82b8e55b6d443e7e19eb90967
SenKU <miquu.official@gmail.com>
2026-02-08 21:31:42 +0700

    Update DEFAULT_SCHED_RAVG_WINDOW value to 13888888

--- kernel/sched/walt.h
+++ kernel/sched/walt.h
@@ -18,10 +18,10 @@
  * Tick interval becomes to 3333333 due to
  * rounding error when HZ=300.
  */
-#define DEFAULT_SCHED_RAVG_WINDOW (16666666)
+#define DEFAULT_SCHED_RAVG_WINDOW (13888888)
 #else
 /* Default window size (in ns) = 12ms */
-#define DEFAULT_SCHED_RAVG_WINDOW 16000000
+#define DEFAULT_SCHED_RAVG_WINDOW 13888888
 #endif
 
 /* Max window size (in ns) = 1s */
