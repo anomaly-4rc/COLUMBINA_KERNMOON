694debcbc633f2af07907457d724c9a16be89448
Anomaly-4rc <miquu.official@gmail.com>
2026-02-21 04:06:37 +0700

    ADD: new IO scheduler(columbina No-Op based

--- drivers/cpufreq/cpufreq_columbina.c
+++ drivers/cpufreq/cpufreq_columbina.c
@@ -427,10 +427,11 @@ static int moon_init(struct dbs_data *dbs_data)
 	if (!tuners)
 		return -ENOMEM;
 
-	tuners->down_differential = 35;
+	tuners->down_differential = 10;
 	tuners->dynamic_threshold_enable = 1;
 	tuners->columbina_mode = 0;
 	tuners->io_is_busy = 1;
+	dbs_data->sampling_rate = MICRO_FREQUENCY_MIN_SAMPLE_RATE;
 	
 	cpu = get_cpu();
 	idle_time = get_cpu_idle_time_us(cpu, NULL);
