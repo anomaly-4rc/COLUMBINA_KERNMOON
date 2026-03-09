091f4d0e2433481351d7ae215bdddaed2a5055d5
SenKU <miquu.official@gmail.com>
2026-03-09 16:17:15 +0000

    script buil

--- b.sh
+++ b.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 set -e
 SECONDS=0
-
 DEFCONFIG="vendor/fog-perf_defconfig"
 CORES=$(nproc --all)
-
 TC_DIR="$(pwd)/tc/gcc-aarch64"
 OUT_DIR="$(pwd)/out"
-# -------------------
-echo "[*] Updating system & installing dependencies..."
-sudo apt-get update -y && sudo apt-get install -y \
-    bc build-essential flex bison lld llvm libssl-dev libelf-dev \
-    ccache libncurses-dev libncurses5
+
+sudo apt-get update || true
+sudo apt-get install -y --no-install-recommends \
+    bc \
+    build-essential \
+    flex \
+    bison \
+    cpio \
+    lld \
+    llvm \
+    libssl-dev \
+    libelf-dev \
+    ccache \
+    libncurses-dev \
+    ncurses-term \
+    git \
+    zip \
+    unzip \
+    curl \
+    ca-certificates \
+    python3 \
+    kmod
 
 if [ ! -d "$TC_DIR" ]; then
-    echo "[*] Toolchain not found! Downloading GCC aarch64..."
     mkdir -p "$TC_DIR"
-
     curl -Ls https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.3-2021.07/binrel/gcc-arm-10.3-2021.07-x86_64-aarch64-none-elf.tar.xz | tar -xJ --strip-components=1 -C "$TC_DIR"
 fi
 
@@ -27,7 +40,6 @@ export CROSS_COMPILE=aarch64-none-elf-
 export KBUILD_BUILD_USER="Filia Lunae🌙"
 
 if [[ "$1" == "-c" || "$1" == "--clean" ]]; then
-    echo "[*] Cleaning kernel output"
     rm -rf "$OUT_DIR"
     make mrproper
     exit 0
@@ -36,11 +48,11 @@ fi
 mkdir -p "$OUT_DIR"
 
 if [[ ! -f "$OUT_DIR/.config" ]]; then
-    echo "[*] Generating defconfig: $DEFCONFIG"
+    echo "[*] defconfig: $DEFCONFIG"
     make O="$OUT_DIR" $DEFCONFIG
 fi
 
-echo "[*] Starting compilation (-j$CORES) with GCC..."
+echo "[*] start... (-j$CORES)..."
 
 make -j$CORES \
      O="$OUT_DIR" \
@@ -50,11 +62,11 @@ KERNEL_IMAGE="$OUT_DIR/arch/arm64/boot/Image.gz"
 
 if [[ -f "$KERNEL_IMAGE" ]]; then
     echo "====================================="
-    echo "   COLUMBINA KERNMOON SUCCESS"
+    echo "   COLUMBINA KERNMOON SUCCESS! 🌙"
     echo "   Time: $((SECONDS/60))m $((SECONDS%60))s"
-    echo "   Output: $KERNEL_IMAGE"
+    echo "        : $KERNEL_IMAGE"
     echo "====================================="
 else
-    echo "!!! Compilation failed. Check $OUT_DIR/build.log"
+    echo "!!!failed. $OUT_DIR/build.log"
     exit 1
 fi
