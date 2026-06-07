2ca4885c012dc6bdbfb46d4ed8c4de41f84154ea
anomaly-4rc <miquu.official@gmail.com>
2026-06-08 00:13:14 +0700

    config: adjust context drawqueue size and drawobj burst parameters

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -14,13 +14,13 @@
 #define DRAWQUEUE_NEXT(_i, _s) (((_i) + 1) % (_s))
 
 /* Number of commands that can be queued in a context before it sleeps */
-static unsigned int _context_drawqueue_size = 100;
+static unsigned int _context_drawqueue_size = 80;
 
 /* Number of milliseconds to wait for the context queue to clear */
 static unsigned int _context_queue_wait = 10000;
 
 /* Number of drawobjs sent at a time from a single context */
-static unsigned int _context_drawobj_burst = 1024;
+static unsigned int _context_drawobj_burst = 2048;
 
 /*
  * GFT throttle parameters. If GFT recovered more than
@@ -35,7 +35,7 @@ static unsigned int _fault_throttle_burst = 3;
  * Maximum ringbuffer inflight for the single submitting context case - this
  * should be sufficiently high to keep the GPU loaded
  */
-static unsigned int _dispatcher_q_inflight_hi = 128;
+static unsigned int _dispatcher_q_inflight_hi = 256;
 
 /*
  * Minimum inflight for the multiple context case - this should sufficiently low
