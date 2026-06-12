d2d35b1e50c1e260683297c5b4354211615b293d
anomaly-4rc <miquu.official@gmail.com>
2026-06-13 00:02:26 +0700

    random: increase pool size for enhanced randomness

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -615,9 +615,9 @@ EXPORT_SYMBOL(get_random_bytes_arch);
  **********************************************************************/
 
 enum {
-	POOL_BITS = BLAKE2S_HASH_SIZE * 8,
-	POOL_READY_BITS = POOL_BITS, /* When crng_init->CRNG_READY */
-	POOL_EARLY_BITS = POOL_READY_BITS / 2 /* When crng_init->CRNG_EARLY */
+	POOL_BITS = (BLAKE2S_HASH_SIZE * 8) * 4,
+	POOL_READY_BITS = POOL_BITS,
+	POOL_EARLY_BITS = POOL_READY_BITS / 2
 };
 
 static struct {
